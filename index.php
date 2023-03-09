7d03662bec7fdc55987b5da2f7719b78e78a6c89
Alexander <formysite.ru@yandex.ru>
2023-03-09 15:17:09 +0300

    start

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Занятия по РНР</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
+</head>
+
+<body>
+    <main>
+        <header>
+            <h1>
+                Урок 7.8. SQL запросы. LEFT JOIN и RIGHT JOIN
+            </h1>
+            <h4>
+                Продолжаем изучать основы SQL и рассмотрим запросы LEFT JOIN и RIGHT JOIN
+            </h4>
+        </header>
+        <section class="code_php">
+            <?php
+            $nickname = "Александр!";
+            $hello = "Привет ";
+            $offer = "Продолжаем в рамках курса РНР изучать основы SQL запросов.<br>
+                      На данном занятии рассмотрим запросы LEFT JOIN и RIGHN JOIN";
+            echo "<h2 class='subtitle-php'>" . $hello . $nickname . "<br>" . "<br>" . $offer . "</h2>";
+            ?>
+        </section>
+        <section class=""></section>
+        <section class=""></section>
+    </main>
+</body>
+
+</html>
